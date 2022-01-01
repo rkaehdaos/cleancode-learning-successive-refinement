15654b734c5052c5f222e7b638bc8454965de82f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-01 18:02:58 +0900

    [#1 1st step] 1st boolean만 되게 작업 중 - 아직 미완

--- src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/Args.java
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/Args.java
@@ -1,8 +1,98 @@
 package me.rkaehdaos.cleancodelearningsuccessiverefinement;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Args {
+    private String schema;
+    private String[] args;
+    private boolean valid;
+    private Set<Character> unexpectedArguments = new TreeSet<>();
+    private Map<Character, Boolean> booleanArgs = new HashMap<>();
+    private int numberOfArguments = 0;
+
+    //생성자
     public Args(String schema, String[] args) {
+        this.schema = schema;
+        this.args = args;
+        this.valid = parse();
     }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    private boolean parse() {
+        if (schema.length() == 0 && args.length == 0)
+            return true;
+        parseSchema();
+        parseArguments();
+        return unexpectedArguments.size() == 0;
+    }
+
+    private boolean parseSchema() {
+        for (String element : schema.split(",m")) {
+            parseSchemaElement(element);
+        }
+        return true;
+    }
+
+    private void parseSchemaElement(String element) {
+        if (element.length() == 1)
+            parseBooleanSchemaElement(element);
+    }
+
+    private void parseBooleanSchemaElement(String element) {
+        char c = element.charAt(0);
+        if (Character.isLetter(c))
+            booleanArgs.put(c, false);
+    }
+
+    private boolean parseArguments() {
+        for (String arg : args)
+            parseArgument(arg);
+        return true;
+
+    }
+
+    private void parseArgument(String arg) {
+        if (arg.startsWith("-"))
+            parseElements(arg);
+    }
+
+    private void parseElements(String arg) {
+        for (int i = 1; i < arg.length(); i++) {
+            parseElement(arg.charAt(i));
+        }
+    }
+
+    private void parseElement(char argChar) {
+        if (isBoolean(argChar)){
+            numberOfArguments++;
+            setBooleanArg(argChar, true);
+        } else
+            unexpectedArguments.add(argChar);
+    }
+
+    private void setBooleanArg(char argChar, boolean value) {
+        booleanArgs.put(argChar, value);
+    }
+    private boolean isBoolean(char argChar) {
+        return booleanArgs.containsKey(argChar);
+    }
+
+    public int cardinality() {
+        return numberOfArguments;
+    }
+
+    public String usage(){
+        if(schema.length()>0) ;
+// TODO 여기부터 p263
+        return "";
+    }
+
 
     public boolean getBoolean(char l) {
         return true;
@@ -25,7 +115,6 @@ public class Args {
         }
         return null;
     }
-
 
 
 }
