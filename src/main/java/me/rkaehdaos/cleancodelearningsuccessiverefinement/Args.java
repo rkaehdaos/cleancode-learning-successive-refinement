ddf831d249f851e0bd710d2adb692809a5485511
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-01 15:32:16 +0900

    일단 에러가 사라질때까지 껍데기 구현

---
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/Args.java
@@ -0,0 +1,18 @@
+package me.rkaehdaos.cleancodelearningsuccessiverefinement;
+
+public class Args {
+    public Args(String schema, String[] args) {
+    }
+
+    public boolean getBoolean(char l) {
+        return true;
+    }
+
+    public int getInt(char c) {
+        return 0;
+    }
+
+    public String getString(char c) {
+        return null;
+    }
+}
---
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/ArgsException.java
@@ -0,0 +1,6 @@
+package me.rkaehdaos.cleancodelearningsuccessiverefinement;
+
+public class ArgsException extends Exception {
+    public String errorMessage() {
+        return "";
+    }
