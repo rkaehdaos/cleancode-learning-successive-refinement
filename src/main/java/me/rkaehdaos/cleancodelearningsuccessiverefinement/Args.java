8f1a0aa07ca6899a70673a8721e56926e2a34391
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-05 10:18:23 +0900

    [#4 Refactoring 파생 분산] 아직도 따로 만든 맵 없애기 중

--- src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/Args.java
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/Args.java
@@ -211,10 +211,16 @@ public class Args {
         return message.toString();
     }
 
-    // TODO : p285부터
     public boolean getBoolean(char arg) {
         ArgumentMarshaler am = booleanArgs.get(arg);
-        return am != null && (Boolean) am.get();
+//        return am != null && (Boolean) am.get();
+        boolean b = false;
+        try {
+            b = am != null && (Boolean) am.get();
+        } catch (ClassCastException e) {
+            b = false;
+        }
+        return b;
     }
 
     public int getInt(char arg) throws ArgsException {
