5baa09aeb36e9994df90238944796e0976898e85
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-05 16:21:46 +0900

    [#4 testcode] main

--- src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/CleancodeLearningSuccessiveRefinementApplication.java
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/CleancodeLearningSuccessiveRefinementApplication.java
@@ -8,7 +8,7 @@ import java.text.ParseException;
 
 public class CleancodeLearningSuccessiveRefinementApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             Args arg = new Args("l,p#,d*", args);
             boolean logging = arg.getBoolean('l');
