7d9932b8bca633fad4aa3141d3d8730bac41fc05
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-05 15:26:54 +0900

    [#4 testcode] testSimpleDoublePresent 추가

--- src/test/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/ArgsTest.java
+++ src/test/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/ArgsTest.java
@@ -1,6 +1,6 @@
 package me.rkaehdaos.cleancodelearningsuccessiverefinement;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +12,6 @@ class ArgsTest {
     public static final Logger log = LoggerFactory.getLogger(ArgsTest.class);
 
     @Test
-    @DisplayName("스키마, 아규먼트 없는 테스트 생성")
     void testCreateWithNoSchemaOrArguments() throws Exception {
         // given
         Args args = new Args("", new String[0]);
@@ -21,7 +20,7 @@ class ArgsTest {
     }
 
     @Test
-    @DisplayName("스키마없이 1개 인자")
+    @Disabled
     void testWithNoSchemaButWithOneArgument() throws Exception {
         try {
             Args args = new Args("", new String[]{"-x"});
@@ -30,6 +29,20 @@ class ArgsTest {
             assertThat(e.getErrorCode()).isEqualTo(ArgsException.ErrorCode.UNEXPECTED_ARGUMENT);
             assertThat(e.getErrorArgumentId()).isEqualTo('x');
         }
+    }
+
+
+    @Test
+    void testSimpleDoublePresent() throws Exception {
+        //given
+        Args args = new Args("x##", new String[]{"-x", "42.3"});
+
+        //then
+        assertThat(args.isValid()).isTrue();
+        assertThat(args.cardinality()).isEqualTo(1);
+        assertThat(args.has('x'));
+        assertThat(args.getDouble('x')).isEqualTo(42.3);
+
     }
 
 }
