669a794af697d29ba28e4a4be574a3b2b738fd4a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-05 15:54:54 +0900

    [#4 testcode] errorMessage() 구현

--- src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/ArgsException.java
+++ src/main/java/me/rkaehdaos/cleancodelearningsuccessiverefinement/ArgsException.java
@@ -18,7 +18,23 @@ public class ArgsException extends Exception {
     }
 
 
-    public String errorMessage() {
+    public String errorMessage() throws Exception {
+        switch(errorCode){
+            case OK:
+                throw new Exception("TILT: Should not get here.");
+            case UNEXPECTED_ARGUMENT:
+                return String.format("인자 -%c 예상치 못함.", errorArgumentId);
+            case MISSING_STRING:
+                return String.format("문자열 파라미터 -%c를 찾을 수 없음.", errorArgumentId);
+            case INVALID_INTEGER:
+                return String.format("Argument -%c는 정수를 기대했지만 %s.", errorArgumentId);
+            case MISSING_INTEGER:
+                return String.format("정수형 파라미터 -%c를 찾을 수 없음.", errorArgumentId);
+            case INVALID_DOUBLE:
+                return String.format("Argument -%c는 double을 기대했지만 %s.", errorArgumentId);
+            case MISSING_DOUBLE:
+                return String.format("double 파라미터 -%c를 찾을 수 없음.", errorArgumentId);
+        }
         return "";
     }
 
